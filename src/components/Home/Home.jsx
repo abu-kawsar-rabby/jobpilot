de68c48c51a28875c798f4c2cb84c67d65d7f8f3
Abu-kawsar-rabby <rabbyafrad24@.com>
2023-04-10 01:47:50 +0600

    main page added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import JobCategorys from '../JobCategorys/JobCategorys';
 import './Home.css';
+import Jobs from '../Jobs/Jobs';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
+            <JobCategorys></JobCategorys>
+            <Jobs></Jobs>
         </div>
     );
 };
