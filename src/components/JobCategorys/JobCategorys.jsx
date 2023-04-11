b0d1cdece1ec42b45f486e6e4e98da37d0736d2b
Abu-kawsar-rabby <rabbyafrad24@.com>
2023-04-12 04:01:57 +0600

    responsive issue fixed

--- src/components/JobCategorys/JobCategorys.jsx
+++ src/components/JobCategorys/JobCategorys.jsx
@@ -10,7 +10,7 @@ const JobCategorys = () => {
         <div className='my-20'>
             <h3 className='text-center text-3xl font-bold text-black'>Job Category List</h3>
             <p className='my-2 text-center'>Explore thousands of job opportunities with all the information you need. Its your future</p>
-            <div className="grid grid-cols-4 mt-10 gap-4">
+            <div className="grid grid-cols-2 md:grid-cols-4 mt-10 gap-4">
                 {
                     datas.slice(0, 4).map(job => <JobCategoryList
                         key={job.id}
--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -18,7 +18,7 @@ const Jobs = () => {
             <p className='text-center my-5'>
                 Explore thousands of job opportunities with all the information you need. Its your future
             </p>
-            <div className="grid grid-cols-2 gap-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
 
                 {
                     showAllJobs ? (
