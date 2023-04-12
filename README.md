2d7c7d349b37bd2900a35c8b04be545587fa6f4f
Abu-kawsar-rabby <rabbyafrad24@.com>
2023-04-12 22:33:59 +0600

    blog ans done

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# b7a9-career-hub-abu-kawsar-rabby
+# JOBPILOT
+
+These websites typically allow users to search and apply for jobs based on different criteria such as job title, industry, location, and experience level.
+
+# LIVE SITE LINK:
+netlify : https://earnest-fairy-fc3b61.netlify.app/
+
+## Features
+
+- Statistic page
+- Blog
+- Applied job
+
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -9,20 +9,20 @@ const Blog = () => {
             ></Header>
             <section className="px-5 md:px-36 mx-auto text-left">
                 <div className="shadow my-2 p-3 rounded">
-                    <h1 className="mt-4 mb-2 text-xl font-bold">1. When should you use context API?</h1>
-                    <h1 className="text-sm"><span className="text-green-600 font-semibold">Answer: </span>anshere.</h1>
+                    <h1 className="mt-4 mb-2 text-xl font-bold">1. When should  use context API?</h1>
+                    <h1 className="text-sm"><span className="text-green-600 font-semibold">Answer: </span>Context API used for access data from many components at different nested levels. </h1>
                 </div>
                 <div className="shadow my-2 p-3 rounded">
                     <h1 className="mt-4 mb-2 text-xl font-bold">2. What is a custom hook?</h1>
-                    <h1 className="text-sm"><span className="text-green-600 font-semibold">Answer: </span>anshere.</h1>
+                    <h1 className="text-sm"><span className="text-green-600 font-semibold">Answer: </span>Custom hook is a hook that create by us for reuseble data that we will use for many different components and it will reduce code repeat and custom hook name start with use....</h1>
                 </div>
                 <div className="shadow my-2 p-3 rounded">
                     <h1 className="mt-4 mb-2 text-xl font-bold">3. What is useRef?</h1>
-                    <h1 className="text-sm"><span className="text-green-600 font-semibold">Answer: </span>ans.</h1>
+                    <h1 className="text-sm"><span className="text-green-600 font-semibold">Answer: </span>useRef is a react build in hook. it will manipulate DOM directly by using ref attribute for particular element.</h1>
                 </div>
                 <div className="shadow my-2 p-3 rounded">
                     <h1 className="mt-4 mb-2 text-xl font-bold">4. What is useMemo?</h1>
-                    <h1 className="text-sm"><span className="text-green-600 font-semibold">Answer: </span>anshere.</h1>
+                    <h1 className="text-sm"><span className="text-green-600 font-semibold">Answer: </span>useMemo hook memorise value on it so that same thing doesn't do every time. that's why application performance and speed boost up.</h1>
                 </div>
 
             </section>
